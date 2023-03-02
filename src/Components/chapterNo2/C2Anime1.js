f2ab62938bec04132ed7fd2b1007492fef9b15a0
민종 <s2hoon326@gmail.com>
2023-03-02 20:11:38 +0900

    챕터 2 연동중입니다

--- src/Components/chapterNo2/C2Anime1.js
+++ src/Components/chapterNo2/C2Anime1.js
@@ -3,7 +3,7 @@ import React from "react";
 const C2Anime1 = () => {
   return (
     <div>
-      <h1>챕터2 - 1번 애니메이션</h1>
+      <h1>챕터2 - 남자 1번 애니메이션</h1>
     </div>
   );
 };
--- src/Components/chapterNo2/Chapter2.js
+++ src/Components/chapterNo2/Chapter2.js
@@ -1,10 +1,97 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
+import "./Chapter2.css";
 
 const ChapterNo2 = () => {
   const dispatch = useDispatch();
+  let [nextBtn, setNextBtn] = useState(0);
+  let [changeBox, setChangeBox] = useState(0);
 
-  return <div>2번 챕터시작-여긴 어디지부터</div>;
+  const DialogFlow = () => {
+    if (nextBtn === 0) {
+      return <Dialog0 />;
+    } else if (nextBtn === 1) {
+      return <Dialog1 />;
+    } else if (nextBtn === 2) {
+      setTimeout(() => {
+        setChangeBox(1);
+      }, 10);
+      return <Dialog2 />;
+    }
+  };
+
+  return (
+    <div className="CP2_UPPER">
+      {DialogFlow()}
+      <button
+        className={"nextBtn" + changeBox}
+        onClick={() => {
+          setNextBtn(nextBtn + 1);
+        }}
+      >
+        다음
+      </button>
+    </div>
+  );
+};
+
+const Dialog0 = () => {
+  return (
+    <>
+      <h3 className="dialog_oneline">. . .</h3>
+    </>
+  );
+};
+
+const Dialog1 = () => {
+  return (
+    <>
+      <h3 className="dialog_oneline">여긴.. 어디지..?</h3>
+    </>
+  );
+};
+
+const Dialog2 = () => {
+  const dispatch = useDispatch();
+  return (
+    <>
+      <div
+        onClick={() => {
+          dispatch({ type: "index/HAPPY_MOOD" });
+        }}
+        className="CH_BOX"
+      >
+        <p>
+          처음 와보는 곳<br />
+          신난다..!
+        </p>
+      </div>
+      <div
+        onClick={() => {
+          dispatch({ type: "index/SAD_MOOD" });
+        }}
+        className="CH_BOX"
+      >
+        <p>
+          낯선 곳은 항상
+          <br />
+          피곤해...
+        </p>
+      </div>
+      <div
+        onClick={() => {
+          dispatch({ type: "index/THRILL_MOOD" });
+        }}
+        className="CH_BOX"
+      >
+        <p>
+          도대체 어디지..
+          <br />
+          긴장돼...
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default ChapterNo2;
